883a0a5da29d7c85ac07dc20d58d6c47696d21cc
rakshith <rakshit.s.gowda@gmail.com>
2021-10-08 20:09:41 +0530

    user post image upload creation

--- controllers/post.js
+++ controllers/post.js
@@ -4,19 +4,19 @@ import cloudinary from 'cloudinary'
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_NAME,
   api_key: process.env.CLOUDINARY_KEY,
-  api_SECRET: process.env.CLOUDINARY_SECRET,
+  api_secret: process.env.CLOUDINARY_SECRET,
 })
 
 export const createPost = async (req, res) => {
   // console.log('post => ', req.body)
-  const { content } = req.body
+  const { content, image } = req.body
   if (!content.length) {
     return res.json({
       error: 'content is required',
     })
   }
   try {
-    const post = new Post({ content, postedBy: req.user._id })
+    const post = new Post({ content, image, postedBy: req.user._id })
     // console.log(`post from try=>`, post)
     post.save()
     res.json(post)
@@ -27,13 +27,15 @@ export const createPost = async (req, res) => {
 }
 
 export const uploadImage = async (req, res) => {
-  console.log('req files => ', req.files)
+  // console.log('req files => ', req.files)
   try {
     const result = await cloudinary.uploader.upload(req.files.image.path)
-    console.log('uploaded image url => result')
+    // console.log('uploaded image url =>', result)
     res.json({
       url: result.secure_url,
       public_id: result.public_id,
     })
-  } catch (error) {}
+  } catch (error) {
+    console.log(error)
+  }
 }
